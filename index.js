934094de5ca03f17801fd6b44a19b36b48a4accb
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-11-10 10:37:39 +0800

    timelog deploy

--- index.js
+++ index.js
@@ -1,41 +1,203 @@
 const moment = require('moment');
 const puppeteer = require('puppeteer');
+const log = require('./log.js');
+const { setIntervalAsync } = require('set-interval-async/fixed');
 
 const schedule = [
   {
     sunday: [
-      { time: '10:00 AM', subject: 'ITME2-BSIT-3WK-LOGIN', reLogin: false },
       {
-        time: '3:00 PM',
-        subject: 'WEB-DESIGN-BSBA-3WK-LOGOUT-LOGIN',
+        time: '10:00 AM',
+        subject: 'ITME2-BSIT-3WK',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '3:10 PM',
+        subject: 'ITME2-BSIT-3WK|WEB-DESIGN-BSBA-3WK',
+        type: 'LOGOUT-LOGIN',
         reLogin: true,
       },
       {
-        time: '8:00 PM',
-        subject: 'WEB-DESIGN-BSBA-3WK-LOGOUT',
+        time: '8:10 PM',
+        subject: 'WEB-DESIGN-BSBA-3WK',
+        type: 'LOGOUT',
         reLogin: false,
       },
     ],
   },
   {
     monday: [
-      { time: '8:00 AM', subject: 'ITME2-BSIT-3WK', reLogin: false },
-      { time: '3:00 PM', subject: 'ITME2-BSIT-3WK', reLogin: true },
+      {
+        time: '8:00 AM',
+        subject: 'WEB-DESIGN-BSBA-3C',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '11:10 AM',
+        subject: 'WEB-DESIGN-BSBA-3C',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+      {
+        time: '12:30 PM',
+        subject: 'WEB-DESIGN-BSBA-3A',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '2:40 PM',
+        subject: 'WEB-DESIGN-BSBA-3A',
+        TYPE: 'LOGOUT',
+        reLogin: false,
+      },
+      {
+        time: '3:00 PM',
+        subject: 'ITME3-BSIT-4A',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '8:10 PM',
+        subject: 'ITME3-BSIT-4A',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+      {
+        time: '8:30 PM',
+        subject: 'WEB-DESIGN-BSBA-3NA',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '10:10 PM',
+        subject: 'WEB-DESIGN-BSBA-3NA',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+    ],
+  },
+  {
+    tuesday: [
+      {
+        time: '8:00 AM',
+        subject: 'WEB-DESIGN-BSBA-3B|C',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '1:10 PM',
+        subject: 'WEB-DESIGN-BSBA-3C|B',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+      {
+        time: '5:00 PM',
+        subject: 'COMPR1-BSIT-1NA',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '8:10 PM',
+        subject: 'COMPR1-BSIT-1NA',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+      {
+        time: '8:30 PM',
+        subject: 'WEB-DESIGN-BSBA-3NA',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '10:10 PM',
+        subject: 'WEB-DESIGN-BSBA-3NA',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+    ],
+  },
+  {
+    wednesday: [
+      {
+        time: '8:00 AM',
+        subject: 'WEB-DESIGN-BSBA-3A',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '11:00 PM',
+        subject: 'WEB-DESIGN-BSBA-3A|3C',
+        type: 'LOGOUT-LOGIN',
+        reLogin: true,
+      },
+      {
+        time: '1:10 PM',
+        subject: 'WEB-DESIGN-BSBA-3C',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+      {
+        time: '2:00 PM',
+        subject: 'ITME2-BSIT-3B',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '7:10 PM',
+        subject: 'ITME2-BSIT-3B',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+    ],
+  },
+  {
+    thursday: [
+      {
+        time: '5:00 PM',
+        subject: 'COMPR1-BSIT-1N',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '7:10 PM',
+        subject: 'COMPR1-BSIT-1N',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
+      {
+        time: '8:00 PM',
+        subject: 'WEB-DESIGN-BSBA-3NA',
+        type: 'LOGIN',
+        reLogin: false,
+      },
+      {
+        time: '10:10 PM',
+        subject: 'WEB-DESIGN-BSBA-3NA',
+        type: 'LOGOUT',
+        reLogin: false,
+      },
     ],
   },
 ];
 
+var prevTime = '';
 async function portalScrap() {
   async function main() {
     let timeToday = moment().format('LT');
-    const today = moment().format('dddd').toLowerCase();
-    const iterator = schedule.entries();
-    for (let e of iterator) {
-      if (e[1][today]) {
-        const res = e[1][today].find((sched) => sched.time === timeToday);
-        if (res) {
-          await proceed(res.reLogin);
-          console.table([res]);
+    let today = moment().format('dddd').toLowerCase();
+    if (timeToday !== prevTime) {
+      prevTime = timeToday;
+      console.log(`Date information: ${today}, ${timeToday}`);
+      let iterator = schedule.entries();
+      for (let e of iterator) {
+        if (e[1][today]) {
+          const res = e[1][today].find((sched) => sched.time === timeToday);
+          if (res) {
+            await proceed(res.reLogin);
+            console.table([res]);
+          }
         }
       }
     }
@@ -75,11 +237,8 @@ async function portalScrap() {
   }
 
   await main();
-  setTimeout(async () => {
-    await main();
-  }, 20000); // 2 minutes
 }
 
-(async () => {
+setIntervalAsync(async () => {
   await portalScrap();
-})();
+}, 10000);
